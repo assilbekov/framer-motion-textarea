7ac874c8cdc714632f4d943a88f5ea687c8b1199
assilbekov <askhat.assilbekov@gmail.com>
2024-03-28 08:22:34 -0300

    init styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useRef, useState } from "react"
+import { AnimatePresence, motion } from "framer-motion"
 
 
 function App() {
-
+  const textareaRef = useRef(null)
+  const [val, setVal] = useState("");
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex flex-col p-5">
+      <textarea className="opacity-0 w-0 h-0" ref={textareaRef} onChange={e => setVal(e.target.value)} />
+      <div className="min-h-60 bg-neutral-900 text-slate-100 font-spaceGrotesk whitespace-pre-wrap text-xl min-w-full p-5 overflow-x-hidden"></div>
+    </main>
   )
 }
 
