6e78fa43ec22378b11b854a04a1dc019a6d0f3ed
assilbekov <askhat.assilbekov@gmail.com>
2024-03-28 08:32:10 -0300

    add rotation

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null)
   const [val, setVal] = useState("");
 
+  const rotateValue = Math.floor(Math.random() * 201) - 100
+
   return (
     <main className="flex flex-col p-5">
       <textarea className="opacity-0 w-0 h-0" ref={textareaRef} onChange={e => setVal(e.target.value)} />
@@ -17,16 +19,17 @@ function App() {
           {val.split("").map((letter, i) => (
             <motion.span
               key={i}
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
+              initial={{ opacity: 0, y: 100, rotate: rotateValue }}
+              animate={{ opacity: 1, y: 0, rotate: 0 }}
               exit={{
                 opacity: 0,
                 y: 100,
                 transition: {
                   duration: 0.5,
                 },
-
+                rotate: -rotateValue,
               }}
+              transition={{ duration: 0.5, ease: "easeIn" }}
               className={letter !== "\n" ? "inline-block mr-0.5" : "inline"}
             >{letter}</motion.span>
           ))}
