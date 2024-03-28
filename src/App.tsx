4c31a9721e59cc84c038ec664a29a30020fc945d
assilbekov <askhat.assilbekov@gmail.com>
2024-03-28 08:27:37 -0300

    add init animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,25 @@ import { AnimatePresence, motion } from "framer-motion"
 
 
 function App() {
-  const textareaRef = useRef(null)
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null)
   const [val, setVal] = useState("");
 
   return (
     <main className="flex flex-col p-5">
       <textarea className="opacity-0 w-0 h-0" ref={textareaRef} onChange={e => setVal(e.target.value)} />
-      <div className="min-h-60 bg-neutral-900 text-slate-100 font-spaceGrotesk whitespace-pre-wrap text-xl min-w-full p-5 overflow-x-hidden"></div>
+      <div
+        className="min-h-60 bg-neutral-900 text-slate-100 font-spaceGrotesk whitespace-pre-wrap text-xl min-w-full p-5 overflow-x-hidden"
+        onClick={() => textareaRef.current!.focus()}
+      >
+        {val.split("").map((letter, i) => (
+          <motion.span 
+          key={i}
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          className={letter !== "\n" ? "inline-block mr-0.5" : "inline"}
+          >{letter}</motion.span>
+        ))}
+      </div>
     </main>
   )
 }
